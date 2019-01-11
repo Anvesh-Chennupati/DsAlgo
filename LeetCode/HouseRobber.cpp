4f1af17624b9776499133cb83068f725a1af44da
Anvesh <anveshchowdary01@gmail.com>
2019-01-11 14:50:23 -0800

    house robber no extra space used

--- LeetCode/HouseRobber.cpp
+++ LeetCode/HouseRobber.cpp
@@ -5,7 +5,7 @@ using namespace std;
  
 class Solution {
 public: 
-     int maxProfit(vector<int> houses){
+     int maxProfitR(vector<int> houses){
          vector<int> tempsol(houses.size(),0);
          for(int i=0;i<houses.size();i++){
              int tempprofit = houses[i];
@@ -17,11 +17,27 @@ public:
          }
          return *max_element(tempsol.begin(),tempsol.end());
      }
+
+     int maxProfitDP(vector<int> nums){
+         int include =0;
+         int exclude = 0;
+
+         for(int j =0;j<nums.size();j++){
+             int temp = include;
+             include = nums[j] + exclude;
+             exclude = max(exclude,temp);
+         }
+
+         return max(include,exclude);  
+     }
+
 };
  
 int main(int argc, char const *argv[]) {
     vector<int> houses = {2,7,9,3,1};
     Solution s1;
-    cout<<"\nMax profit: "<<s1.maxProfit(houses);
+    cout<<"\nMax profitR: "<<s1.maxProfitR(houses);
+
+    cout<<"\nMax profitDP: "<<s1.maxProfitDP(houses);
     return 0;
 }

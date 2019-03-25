b06ecd7535a656ba1df48d5b8430161ed925cf3d
Anvesh <anveshchowdary01@gmail.com>
2019-03-24 20:33:25 -0700

    updates

---
+++ DataStructures/Trees/RatingofProduct.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+
+using namespace std;
+
+class TreeNode{
+    public:
+    int id;
+    double rating;
+    TreeNode(int id,double rating){
+        this->id = id;
+        this->rating = rating;
+    }
+};
+
+
+int main(){
+    
+    return 0;
+}

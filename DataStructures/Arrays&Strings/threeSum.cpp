d2a1e2b299311f6afa7c62f55f63b0a902e9d39b
Anvesh <anveshchowdary01@gmail.com>
2019-03-24 23:36:30 -0700

    ThreeSum

---
+++ DataStructures/Arrays&Strings/threeSum.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int> > threeSum(vector<int> &num){
+        vector<vector<int> > res;
+
+    std::sort(num.begin(), num.end());
+
+    for (int i = 0; i < num.size(); i++) {
+        
+        int target = -num[i];
+        int front = i + 1;
+        int back = num.size() - 1;
+
+        while (front < back) {
+
+            int sum = num[front] + num[back];
+            
+            if (sum < target)
+                front++;
+
+            else if (sum > target)
+                back--;
+
+            else {
+                vector<int> triplet(3, 0);
+                triplet[0] = num[i];
+                triplet[1] = num[front];
+                triplet[2] = num[back];
+                res.push_back(triplet);
+                
+                while (front < back && num[front] == triplet[1]) front++;
+
+                while (front < back && num[back] == triplet[2]) back--;
+            }
+            
+        }
+
+        while (i + 1 < num.size() && num[i + 1] == num[i]) 
+            i++;
+
+    }
+    
+    return res;
+        
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    Solution s1;
+    for(auto x:s1.threeSum(nums)){
+        cout<<endl;
+        for(auto y:x){
+            cout<<y<<" ";
+        }
+    }
+    return 0;
+}
--- LeetCode/HouseRobber.cpp
+++ LeetCode/HouseRobber.cpp
@@ -1,43 +1,49 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
- 
-class Solution {
-public: 
-     int maxProfitR(vector<int> houses){
-         vector<int> tempsol(houses.size(),0);
-         for(int i=0;i<houses.size();i++){
-             int tempprofit = houses[i];
-             for(int j= i+2;j<houses.size();j+=2){
-                 tempprofit += houses[j];
-             }
 
-             tempsol[i] = tempprofit;
-         }
-         return *max_element(tempsol.begin(),tempsol.end());
-     }
+class Solution
+{
+public:
+    int maxProfitR(vector<int> houses)
+    {
+        vector<int> tempsol(houses.size(), 0);
+        for (int i = 0; i < houses.size(); i++)
+        {
+            int tempprofit = houses[i];
+            for (int j = i + 2; j < houses.size(); j += 2)
+            {
+                tempprofit += houses[j];
+            }
 
-     int maxProfitDP(vector<int> nums){
-         int include =0;
-         int exclude = 0;
+            tempsol[i] = tempprofit;
+        }
+        return *max_element(tempsol.begin(), tempsol.end());
+    }
 
-         for(int j =0;j<nums.size();j++){
-             int temp = include;
-             include = nums[j] + exclude;
-             exclude = max(exclude,temp);
-         }
+    int maxProfitDP(vector<int> nums)
+    {
+        int include = 0;
+        int exclude = 0;
 
-         return max(include,exclude);  
-     }
+        for (int j = 0; j < nums.size(); j++)
+        {
+            int temp = include;
+            include = nums[j] + exclude;
+            exclude = max(exclude, temp);
+        }
 
+        return max(include, exclude);
+    }
 };
- 
-int main(int argc, char const *argv[]) {
-    vector<int> houses = {2,7,9,3,1};
-    Solution s1;
-    cout<<"\nMax profitR: "<<s1.maxProfitR(houses);
 
-    cout<<"\nMax profitDP: "<<s1.maxProfitDP(houses);
+int main(int argc, char const *argv[])
+{
+    vector<int> houses = {2, 7, 9, 3, 1};
+    Solution s1;
+    cout << "\nMax profitR: " << s1.maxProfitR(houses);
+
+    cout << "\nMax profitDP: " << s1.maxProfitDP(houses);
     return 0;
 }

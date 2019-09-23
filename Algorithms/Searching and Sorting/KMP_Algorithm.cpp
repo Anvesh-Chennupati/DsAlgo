0364a88905d4ddd153b6b47d8b43e53466082ea0
Anvesh <anveshchowdary01@gmail.com>
2019-09-22 17:13:34 -0700

    KMP-Algo working

--- Algorithms/Searching and Sorting/KMP_Algorithm.cpp
+++ Algorithms/Searching and Sorting/KMP_Algorithm.cpp
@@ -1,17 +1,67 @@
 #include<iostream>
 #include<vector>
+#include<cstring>
+
 
 using namespace std;
 
-void inc(int* arr,int n){
-    for(int i=0; i<n;i++){
-        arr[i]++;
+void computeLPS(int* lps,string pattern){
+    int len =0;
+    int i = 1;
+    lps[0] = 0;
+    while(i <pattern.length()){
+        if(pattern[i] == pattern[len]){
+            len++;
+            lps[i] = len;
+            i++;
+        }
+        else{
+            if(len!=0){
+                len = lps[len -1];
+            }
+            else{
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+}
+
+void PatternSearchKMP(string sequence, string pattern){
+    if(sequence.length() < pattern.length()) cout<<"Invalid Input";
+    
+    //creating a lps table to track pattern occurences
+    int lps[pattern.length()];
+    memset(lps,0,sizeof(lps));
+    computeLPS(lps,pattern);
+    cout<<"\nComputed LPS ";
+    for(auto x:lps) cout<<x<<" ";
+
+    int i =0;
+    int j =0;
+    while(i < sequence.length()){
+        if(sequence[i] == pattern[j]){
+            i++;
+            j++;
+        }
+        if(j == pattern.length()){
+            cout<<"\nPattern found at Index: "<<i-j;
+            j = lps[j-1];
+        }
+
+        else if (sequence[i]!= pattern[j] && i< sequence.length()){
+            if(j!= 0){
+                j = lps[j-1];
+            }
+            else{
+                i++;
+            }
+        }
     }
 }
 
 int main(int argc,char** argv){
-    int arr[] = {1,2,3,4,5};
-    inc(arr,sizeof(arr)/sizeof(arr[0]));
-    for(auto x: arr) cout<<x<<" ";
-    return 0;
+    string sequence = "ABABDABACDABABCABAB";
+    string pattern = "ABABCABAB";
+    PatternSearchKMP(sequence,pattern);
 }

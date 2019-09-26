1de33c42bf632cedd32887b226c0ed7c8f120795
Anvesh <anveshchowdary01@gmail.com>
2019-09-25 17:04:10 -0700

    tresure Island_1

--- DataStructures/Graphs/tresure_island_I.cpp
+++ DataStructures/Graphs/tresure_island_I.cpp
@@ -42,29 +42,29 @@ int minimumStep(vector<vector<char> > grid){
     q.push_back(Point(0,0));
     grid[0][0] = 'D';
     for (int steps = 1; !q.empty(); steps++) {
-        cout<<"\nsteps value: "<<steps;
+        /*cout<<"\nsteps value: "<<steps;                     //fordebugging
         cout<<"\nitems in current queue: ";
         list<Point>::iterator it;
         for(it = q.begin(); it!= q.end();it++){
             Point temp = *it;
             temp.PrintPoint();
-        }
-            for (int sz = q.size(); sz > 0; sz--) {
-                Point p = q.front();
-                q.pop_front();
-                for (auto x : directions) {
-                    int r = p.x + x[0];
-                    int c = p.y + x[1];
-                    
-                    if (isSafe(grid, r, c)) {
-                        if (grid[r][c] == 'X') return steps;
-                        grid[r][c] = 'D';
-                        q.push_back(Point(r, c));
-                    }
+        }*/
+        for (int sz = q.size(); sz > 0; sz--) {
+            Point p = q.front();
+            q.pop_front();
+            for (auto x : directions) {
+                int r = p.x + x[0];
+                int c = p.y + x[1];
+                
+                if (isSafe(grid, r, c)) {
+                    if (grid[r][c] == 'X') return steps;
+                    grid[r][c] = 'D';
+                    q.push_back(Point(r, c));
                 }
             }
         }
-        return -1;
+    }
+    return -1;
 }
 
 int main(int argc, char** argv){

0c86e8f66a0e16d1e81d87f5abb3c0e1c6f492b0
Anvesh-Chennupati <anveshchowdary01@gmail.com>
2020-05-12 12:13:44 -0700

    Log(n)

---
+++ LeetCode/SingleElementSortedArray.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution{
+    public:
+    int singleNonDuplicate(vector<int> nums){
+        int lo = 0;
+        int hi = nums.size() - 1;
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            bool halvesAreEven = (hi - mid) % 2 == 0;
+            if (nums[mid + 1] == nums[mid]) {
+                if (halvesAreEven) {
+                    lo = mid + 2;
+                } else {
+                    hi = mid - 1;
+                }
+            } else if (nums[mid - 1] == nums[mid]) {
+                if (halvesAreEven) {
+                    hi = mid - 2;
+                } else {
+                    lo = mid + 1;
+                }
+            } else {
+                return nums[mid];
+            }
+        }
+        return nums[lo];
+    }
+
+};
+
+int main(){
+    Solution s;
+    vector<int> t1{1,1,2,2,3,3,4,8,8};
+    vector<int> t2{3,3,7,7,10,11,11};
+    cout<<s.singleNonDuplicate(t1)<<endl;
+    cout<<s.singleNonDuplicate(t2)<<endl;
+}
